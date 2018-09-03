2d7cd5669717761de16fc83fe9fa10c2c2cf3518
Admin <Admin@192.168.1.102>
2018-09-03 16:09:08 +0530

    Well this is my first assignment where i'm trying print specified pattern. 1 12 123 1234 12345 123456

---
+++ My Assignments/src/Pattern1.java
@@ -0,0 +1,27 @@
+
+public class Pattern1 {
+
+	public static void main(String[] args) 
+	{
+	
+	int i=1;
+	
+		for(i=1;i<=6;i++)
+		{
+			for(int j=1;j<=6;j++)
+			{
+				if(j<=i)
+				{
+					System.out.print(j);
+				}
+				/*else
+				{
+					System.out.print(" ");
+				}  */
+			}
+		  System.out.println("");
+
+		
+		}
+	}
+}
